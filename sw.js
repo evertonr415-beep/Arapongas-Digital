93733eda7396cd1f5209a9538111c7587c1f4dbe
evertonr415-beep <evertonr415@gmail.com>
2026-09-16 06:50:26 -0300

    feat: adiciona service worker PWA

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='ad-v1';const ASSETS=['/','/index.html','/assets/app.css','/js/app.js'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
